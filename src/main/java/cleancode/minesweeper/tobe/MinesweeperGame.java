f336c81c31b99626325749c44e7344c437598070
yoon <hunyoon435@gmail.com>
2024-10-09 18:27:45 +0900

    refactor: extract domain
    Readable Code 강의 섹션 5 - 6

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -1,19 +1,19 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.Advanced;
-import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
-import cleancode.minesweeper.tobe.io.InputHandler;
-import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class MinesweeperGame {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new Advanced();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
-        MineSweeper mineSweeper = new MineSweeper(gameLevel, inputHandler, outputHandler);
+        GameConfig gameConfig = new GameConfig(
+                new Advanced(),
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler());
+
+        MineSweeper mineSweeper = new MineSweeper(gameConfig);
         mineSweeper.initialize();
         mineSweeper.run();
     }
