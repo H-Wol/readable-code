bbe7b9a6184738d3aace283ca62d072623c9274f
yoon <hunyoon435@gmail.com>
2024-10-09 19:29:25 +0900

    refactor: change the structure of a package
    Readable Code 강의 섹션 6 - 3

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellSnapshot.java
@@ -0,0 +1,73 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public class CellSnapshot {
+    private final CellSnapshotStatus status;
+    private final int nearbyLandMineCount;
+
+    private CellSnapshot(CellSnapshotStatus status, int nearbyLandMineCount) {
+        this.status = status;
+        this.nearbyLandMineCount = nearbyLandMineCount;
+    }
+
+    public static CellSnapshot of(CellSnapshotStatus status, int nearbyLandMineCount) {
+        return new CellSnapshot(status, nearbyLandMineCount);
+    }
+
+    public static CellSnapshot ofEmpty() {
+        return of(CellSnapshotStatus.EMPTY, 0);
+    }
+
+    public static CellSnapshot ofFlag() {
+        return of(CellSnapshotStatus.FLAG, 0);
+    }
+
+    public static CellSnapshot ofNumber(int nearbyLandMineCount) {
+        return of(CellSnapshotStatus.NUMBER, nearbyLandMineCount);
+    }
+
+    public static CellSnapshot ofLandmine() {
+        return of(CellSnapshotStatus.LAND_MINE, 0);
+    }
+
+    public static CellSnapshot ofUnchecked() {
+        return of(CellSnapshotStatus.UNCHECKED, 0);
+    }
+
+    public boolean isSameStatus(CellSnapshotStatus cellSnapshotStatus) {
+        return this.status == cellSnapshotStatus;
+    }
+
+    public CellSnapshotStatus getStatus() {
+        return status;
+    }
+
+    public int getNearbyLandMineCount() {
+        return nearbyLandMineCount;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + nearbyLandMineCount;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CellSnapshot other = (CellSnapshot) obj;
+        if (status != other.status)
+            return false;
+        if (nearbyLandMineCount != other.nearbyLandMineCount)
+            return false;
+        return true;
+    }
+
+}
---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/io/sign/CellSignFinder.java
@@ -0,0 +1,23 @@
+package cleancode.minesweeper.tobe.minesweeper.io.sign;
+
+import java.util.List;
+
+import cleancode.minesweeper.tobe.minesweeper.board.cell.CellSnapshot;
+
+public class CellSignFinder {
+    private static final List<CellSignProvidable> cellSignProviders = List.of(
+            new EmptyCellSignProvider(),
+            new FlagCellSignProvider(),
+            new LandMineCellSignProvider(),
+            new NumberCellSignProvider(),
+            new UncheckedCellSignProvider());
+
+    public String findCellSignFrom(CellSnapshot snapshot) {
+        return cellSignProviders.stream()
+                .filter(provider -> provider.supports(snapshot))
+                .findFirst()
+                .map(provider -> provider.provide((snapshot)))
+                .orElseThrow(() -> new IllegalArgumentException("확인할 수 없는 코드입니다."));
+
+    }
+}
